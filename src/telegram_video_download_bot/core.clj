6334854fce6fad0e6dc2719acf29216bb61b82c5
Napuu <santeri.kaariainen@iki.fi>
2021-12-22 00:06:21 +0200

    debug

--- src/telegram_video_download_bot/core.clj
+++ src/telegram_video_download_bot/core.clj
@@ -29,14 +29,17 @@
   (h/message-fn
    (fn [{{id :id} :chat :as message}]
      (println "received message")
-
+     (println message)
+     (def message_id (:message_id message))
      (def patterns
        (list
+        #"https\:\/\/vm.tiktok.com\/[a-zA-Z0-9]+"
         #"https\:\/\/www.youtube.com/watch\?v=[a-zA-Z0-9-_]+"
         #"https\:\/\/youtu.be/[a-zA-Z0-9-_]+"))
 
      ;(def text "https://youtu.be/e6MLjaKhp5U")
      (def text (:text message))
+     (if (not (nil? text)) (
      (def matches
        (filter (fn [x] (not (nil? x)))
                (flatten
@@ -44,20 +47,26 @@
                  (fn [pattern] [(re-seq pattern text)])
                  patterns))))
 
+     (if (> (count matches) 0)
+(
      (def filenames
        (mapv (fn [url]
-               (def filename (str/trim (:out (sh "youtube-dl" "--get-filename" url))))
+               (println "youtube-dl query next")
+               (def filename (str/trim (:out (sh "youtube-dl" "-f" "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" "--get-filename" url))))
+               (println "youtube-dl query done")
                (if (.exists (io/file filename))
                  (println "File already exists: " filename)
-                 (sh "youtube-dl" "-o" filename url))
-               (sh "mv" filename target-dir)
+                 (sh "youtube-dl" "-f" "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" "-o" filename url))
+               (sh "cp" filename target-dir)
                filename)
              matches))
 
      (run! (fn [filename]
              (t/send-video token id (apply str [base-url filename]))) filenames)
 
-     )))
+     ) (println "non matching message")))
+        (println "nil")))
+))
 
 
 (defn -main
