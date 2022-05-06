baf3326e9336cc429b8d505865e005af47adbfa9
Santeri Kääriäinen <santeri.kaariainen@iki.fi>
2022-05-06 11:45:49 +0300

    downloading part is now a bit better

--- src/telegram_video_download_bot/downloader.clj
+++ src/telegram_video_download_bot/downloader.clj
@@ -11,41 +11,52 @@
   (let [json-string "{\"kissa\": 123}"]
     (:kissa (json/read-str json-string :key-fn keyword))))
 
+(defn handle-successful-download
+  [token chat-id reply-to-id file-location message-id base-error-message]
+  (let [status-code (send-telegram-command {:bot-token   token
+                                            :chat-id     chat-id
+                                            :method      "sendVideo"
+                                            :reply-to-id reply-to-id
+                                            :file        file-location})]
+    (if (= status-code 200)
+      (do (log/info "File sent successfully")
+          (send-telegram-command {:bot-token          token
+                                  :chat-id            chat-id
+                                  :method             "deleteMessage"
+                                  :deleted-message-id message-id}))
+      (do
+        (log/error "Something went wrong while trying to send file")
+        (send-telegram-command {:bot-token   token
+                                :chat-id     chat-id
+                                :method      "sendMessage"
+                                :reply-to-id reply-to-id
+                                :text        (str base-error-message " (" status-code ")")})))))
+
+(defn handle-unsuccessful-download
+  [token chat-id message-id base-error-message]
+  (send-telegram-command {:bot-token   token
+                          :chat-id     chat-id
+                          :method      "sendMessage"
+                          :reply-to-id message-id
+                          :text        base-error-message}))
+
 (defn message-handler
-  [_ _  ^bytes payload]
+  [_ _ ^bytes payload]
   (log/info "Received message")
-  (let [parsed (json/read-str (String. payload "UTF-8"))
+  (let [{link :link
+         chat-id :chat-id
+         message-id :message-id
+         reply-to-id :reply-to-id} (json/read-str (String. payload "UTF-8") :key-fn keyword)
         token (get-config-value :token)
-        link (get parsed "link")
-        chat-id (get parsed "chat-id")
-        _ (send-telegram-command {:bot-token token :chat-id chat-id :method "sendChatAction" :action "upload_video" })
-        message-id (get parsed "message-id")
-        reply-to-id (get parsed "reply-to-id" nil)
-        file-location (download-file link "/tmp")]
-    (if (nil? file-location)
-      (send-telegram-command {:bot-token token
-                              :chat-id chat-id
-                              :method "sendMessage"
-                              :reply-to-id message-id
-                              :text (get-config-value :base-error-message)})
-      (let [status-code (send-telegram-command {:bot-token token
-                                               :chat-id chat-id
-                                               :method "sendVideo"
-                                               :reply-to-id reply-to-id
-                                               :file file-location})]
-        (if (= status-code 200)
-          (do (log/info "File sent successfully")
-              (send-telegram-command {:bot-token token
-                                      :chat-id chat-id
-                                      :method "deleteMessage"
-                                      :deleted-message-id message-id}))
-          (do
-            (log/error "Something went wrong while trying to send file")
-            (send-telegram-command {:bot-token token
-                                    :chat-id chat-id
-                                    :method "sendMessage"
-                                    :reply-to-id reply-to-id
-                                    :text (str (get-config-value :base-error-message) "(" status-code ")")})))))))
+        base-error-message (get-config-value :base-error-message)]
+
+    (log/info "Starting file download")
+    (send-telegram-command {:bot-token token :chat-id chat-id :method "sendChatAction" :action "upload_video"})
+
+    (let [file-location (download-file link "/tmp")]
+      (if (nil? file-location)
+        (handle-unsuccessful-download token chat-id message-id base-error-message)
+        (handle-successful-download token chat-id reply-to-id file-location message-id base-error-message)))))
 
 (defn start-downloader []
   (let [{:keys [:ch :qname]} @@global-mq-connection]
