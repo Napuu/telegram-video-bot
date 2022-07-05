b30ce503b3d7ef01ef34bc51f8f49af89c98e6b7
Santeri Kääriäinen <santeri.kaariainen@iki.fi>
2022-07-05 19:56:46 +0300

    limit max file size to avoid 413 from telegram
    using very rudimentary approach, -fs flag of ffmpeg,
    so writing is simply stopped after reaching certain size

--- src/telegram_video_download_bot/util.clj
+++ src/telegram_video_download_bot/util.clj
@@ -1,9 +1,13 @@
 (ns telegram-video-download-bot.util
   (:require [clojure.java.shell :refer [sh]]
+            [clojure.java.io :as io]
             [clojure.string :as str]
             [clj-http.client :as client]
             [clojure.tools.logging :as log]
             [telegram-video-download-bot.config :refer [get-config-value]]))
+
+(def max-size-in "500m")
+(def max-size-out 45000000)
 
 (defn filename-to-full-path
   "Full path to the file"
@@ -32,7 +36,7 @@
 
 (def yt-dlp-base-args ["yt-dlp" 
                        "--merge-output-format" "mp4"
-                       "--max-filesize" "200m"
+                       "--max-filesize" max-size-in
                        "-S" "codec:h264"])
 
 (defn get-redirect-url
@@ -54,30 +58,34 @@
   (-> (apply sh (concat yt-dlp-base-args ["-o" output-path url] (if try-additional-args yt-dlp-additional-args "")))
       :exit))
 
-(defn handle-non-mp4
-  "Check that file is actually mp4. Returns path that is guaranteed to be mp4, nil on fail.
+(defn file-too-big [filename]
+  (> (.length (io/file filename)) max-size-out))
+
+(defn run-ffmpeg
+  "Check that file is actually mp4 and not too big. Returns path that is guaranteed to be mp4 of proper size, nil on fail.
 
   Even though we specifically request mp4-file, something else,
   usually webm-file might be downloaded as well. In that scenario,
-  it's converted to mp4 via ffmpeg."
+  it's converted to mp4 via ffmpeg. '-fs'-flag is used to stop writing after file is big enough."
   [output-path]
 
-  (if (str/ends-with? output-path ".mp4")
-    output-path
-    (let [new-output-path (str output-path ".mp4")
-          ffmpeg-exit-code (-> (sh "ffmpeg" "-y" "-i" output-path new-output-path) :exit)]
-      (when (= ffmpeg-exit-code 0)
-        new-output-path))))
+  (let [too-big? (file-too-big output-path)]
+    (if (and (str/ends-with? output-path ".mp4") (not too-big?))
+      output-path
+      (let [new-output-path (str output-path "_new.mp4")
+            ffmpeg-output (sh "ffmpeg" "-y" "-i" output-path "-fs" (str max-size-out) new-output-path)
+            ffmpeg-exit-code (:exit ffmpeg-output)]
+        (when (= ffmpeg-exit-code 0)
+          new-output-path)))))
 
 (defn download-file
   "Download file and return its locations on disk. Return nil on fail."
   [url target-dir try-additional-args]
   (log/info "Downloading file")
   (when-let [filename (str (java.util.UUID/randomUUID) ".mp4")]
-    (let [full-path (filename-to-full-path target-dir filename)
-          yt-dlp-exit-code (yt-dlp-download-file full-path url try-additional-args)]
-      (when (= yt-dlp-exit-code 0)
-        (handle-non-mp4 full-path)))))
+    (let [full-path (filename-to-full-path target-dir filename)]
+      (yt-dlp-download-file full-path url try-additional-args)
+      (run-ffmpeg full-path))))
 
 (comment
   (get-redirect-url "asdf"))
