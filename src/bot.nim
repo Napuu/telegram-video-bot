6f6419265ec9b56755482f7af28cf02dd9415ff5
Santeri Kääriäinen <santeri.kaariainen@iki.fi>
2023-03-14 14:14:20 +0200

    more log stuff

--- src/bot.nim
+++ src/bot.nim
@@ -2,7 +2,8 @@ import std/[sugar, locks, options, math, logging, strutils, os, threadpool, json
 import client, util
 
 let levelThreshold = getLogLevel()
-var L = newConsoleLogger(levelThreshold=levelThreshold, fmtStr="$levelname, [$time] ")
+let L = newConsoleLogger(levelThreshold=levelThreshold, fmtStr="$levelname, [$time] ")
+addHandler(L)
 
 echo "Init logger for level ", levelThreshold
 
@@ -19,7 +20,7 @@ proc handleVideoThreaded(chatId: int, url: string, msgId: int, replyToMsgId: Opt
         let runnersAlive = collect(for v in chatsSending.values: v).sum
         if runnersAlive < CONCURRENCY_LIMIT:
           break
-        L.log(lvlNotice, "waiting for free worker... ", $runnersAlive)
+        notice("waiting for free worker... ", $runnersAlive)
       sleep(2000)
 
     withLock chatsSendingLock:
@@ -31,33 +32,33 @@ proc handleVideoThreaded(chatId: int, url: string, msgId: int, replyToMsgId: Opt
     var resp: Option[JsonNode]
     resp = telegramRequest("sendChatAction", %*{"chat_id": chatId, "action": "upload_video"})
     if resp.isOk:
-      L.log(lvlDebug, "Initial status msg sent successfully")
+      debug("Initial status msg sent successfully")
     else:
-      L.log(lvlWarn, "Sending initial status failed")
+      warn("Sending initial status failed")
     let (tmpFile, statusCode) = downloadVideo(url)
 
     if statusCode == 0:
-      L.log(lvlDebug, "Downloaded video successfully")
+      debug("Downloaded video successfully")
       let fullFilePath = getAppDir() & "/" & tmpFile
       let dimensions = getVideoDimensions(fullFilePath)
       var videoOptions = %*{"chat_id": chatId, "width": dimensions[0], "height": dimensions[1]}
       resp = telegramVideoRequest(videoOptions, fullFilePath)
       if resp.isOk:
-        L.log(lvlDebug, "Sending video succeeded")
+        debug("Sending video succeeded")
       else:
-        L.log(lvlWarn, "Sending video failed")
+        warn("Sending video failed")
       resp = telegramRequest("deleteMessage", %*{"chat_id": chatId, "message_id": msgId})
       if resp.isOk:
-        L.log(lvlDebug, "Deleting message succeeded")
+        debug("Deleting message succeeded")
       else:
-        L.log(lvlWarn, "Deleting message failed")
+        warn("Deleting message failed")
     else:
-      L.log(lvlWarn, "Downloading video failed, url: ", url)
+      warn("Downloading video failed, url: ", url)
       resp = telegramRequest("sendMessage", %*{"chat_id": chatId, "text": "Hyvä linkki..."})
       if resp.isOk:
-        L.log(lvlDebug, "Sending message succeeded")
+        debug("Sending message succeeded")
       else:
-        L.log(lvlWarn, "Deleting message failed")
+        warn("Deleting message failed")
 
     withLock chatsSendingLock:
       chatsSending[chatId].dec
@@ -70,9 +71,9 @@ proc sendingStatusPoller(): void {.gcsafe.} =
           if (chatsSending[chatId] > 0):
             let resp = telegramRequest("sendChatAction", %*{"chat_id": chatId, "action": "upload_video"})
             if resp.isOk:
-              L.log(lvlDebug, "Polling status msg sent successfully")
+              debug("Polling status msg sent successfully")
             else:
-              L.log(lvlWarn, "Sending polling status failed")
+              warn("Sending polling status failed")
       sleep 4000
   
 proc handleUpdate(update: JsonNode): void =
@@ -84,7 +85,7 @@ proc handleUpdate(update: JsonNode): void =
       msgId = msg["message_id"].getInt
       replyToMsgId = if msg.hasKey("reply_to_message"): some(msg["reply_to_message"]["message_id"].getInt) else: none(int)
       endingString = " dl"
-    L.log(lvlDebug, "Received update")
+    debug("Received update")
     if msgText.endsWith(endingString):
       let url = msgText[0..^(endingString.len+1)]
       spawn handleVideoThreaded(chatId, url, msgId, replyToMsgId)
